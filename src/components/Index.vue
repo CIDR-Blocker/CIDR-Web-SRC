91aee61f4d7234d7749e28acdb3e580a877ac55b
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-13 22:49:00 -0400

    Updated Link

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -10,11 +10,11 @@
           </div>
           <div class="has-text-centered">
             <h1 class="title animated fadeIn">Blocks CIDR (Classless Inter-Domain Routing) IP Ranges</h1>
-            <a class="button is-primary is-large" href="https://github.com/RumbleFrog/CIDR-Blocker/releases" target="_blank">
+            <a class="button is-primary is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/releases" target="_blank">
               <b-icon icon="download"></b-icon>
                 <span>Download</span>
               </a>
-            <a class="button is-info is-large" href="https://github.com/RumbleFrog/CIDR-Blocker/" target="_blank">
+            <a class="button is-info is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/" target="_blank">
               <b-icon icon="github"></b-icon>
               <span>Github</span>
             </a>
