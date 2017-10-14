e496f1f26cb6562d5e2cd548ce9ade0f64f9128c
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-14 12:48:34 -0400

    TS: Could not find driver mysql

--- src/components/Documentation/Setup/Troubleshooting.vue
+++ src/components/Documentation/Setup/Troubleshooting.vue
@@ -13,7 +13,64 @@
         </header>
         <div class="card-content">
           <div class="content">
-            Test
+            <ol>
+              <li>
+                Make sure <code>dbi.mysql.ext</code> exists within Sourcemod's extensions folder, and check if it's loaded using <code>sm exts list</code>
+              </li>
+              <li>
+                <b-tag type="is-success">Linux</b-tag> Make sure it's executable via <code>chmod u+x dbi.mysql.ext.so</code>
+              </li>
+              <li>
+                <b-tag type="is-success">Linux</b-tag> Install <code>zlib</code>
+                <ul style="list-style-type:none">
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">Debian/Ubuntu</b-tag>
+                      <b-tag type="is-info">32 bit</b-tag>
+                      <b-tag type="is-danger">apt-get install zlib1g</b-tag>
+                    </b-taglist>
+                  </li>
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">Debian/Ubuntu</b-tag>
+                      <b-tag type="is-info">64 bit</b-tag>
+                      <b-tag type="is-danger">apt-get install lib32z1</b-tag>
+                    </b-taglist>
+                  </li>
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">Mandriva</b-tag>
+                      <b-tag type="is-info">32/64 bit</b-tag>
+                      <b-tag type="is-danger">urpmi zlib1</b-tag>
+                    </b-taglist>
+                  </li>
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">Fedora</b-tag>
+                      <b-tag type="is-info">32/64 bit</b-tag>
+                      <b-tag type="is-danger">yum install zlib.i686</b-tag>
+                    </b-taglist>
+                  </li>
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">SUSE</b-tag>
+                      <b-tag type="is-info">32 bit</b-tag>
+                      <b-tag type="is-danger">zypper install libz1</b-tag>
+                    </b-taglist>
+                  </li>
+                  <li>
+                    <b-taglist attached>
+                      <b-tag type="is-black">SUSE</b-tag>
+                      <b-tag type="is-info">64 bit</b-tag>
+                      <b-tag type="is-danger">zypper install libz1-32bit</b-tag>
+                    </b-taglist>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <b-tag type="is-success">Linux</b-tag> Use <code>ldd -d -r dbi.mysql.ext.so</code> to look for missing dependencies
+              </li>
+            </ol>
           </div>
         </div>
       </div>
