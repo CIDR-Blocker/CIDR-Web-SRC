1eb63e5a74fc292956338f1e335e6ce08a816fe3
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-14 00:14:02 -0400

    Troubleshooting Route

--- src/components/Documentation/Setup/Installation.vue
+++ src/components/Documentation/Setup/Installation.vue
@@ -13,7 +13,7 @@
           Create a <code>cidr_blocker</code> section within Sourcemod's <code>databases.cfg</code> config with your database parameters.
         </li>
         <li>
-          Optionally but recommended, to block most VPNs, import <a href="https://github.com/CIDR-Blocker/CIDR-Blocker/blob/master/imports/datacenters.sql">datacenters.sql</a> into <code>cidr_list</code> table.
+          Optionally but recommended, to block most VPNs: import <a href="https://github.com/CIDR-Blocker/CIDR-Blocker/blob/master/imports/datacenters.sql">datacenters.sql</a> into <code>cidr_list</code> table.
         </li>
       </ol>
     </div>
