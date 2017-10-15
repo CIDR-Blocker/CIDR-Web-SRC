25ae0e6d64fae17c50e9e985393ae75572d323eb
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-15 17:08:23 -0400

    Removed Extra App

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,26 +1,24 @@
 <template>
-  <div id="app">
-    <section class="hero is-fullheight">
-      <div class="hero-body">
-        <div class="container">
-          <div class="columns">
-            <div class="column has-text-centered">
-              <img class="animated fadeInDown" src="/static/favicon/favicon.png" height="250" width="250" alt="CIDR Blocker" style="margin-left:50px" />
-            </div>
-          </div>
-          <div class="has-text-centered">
-            <h1 class="title animated fadeIn">Blocks CIDR (Classless Inter-Domain Routing) IP Ranges</h1>
-            <a class="button is-primary is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/releases" target="_blank">
-              <b-icon icon="download"></b-icon>
-                <span>Download</span>
-              </a>
-            <a class="button is-info is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/" target="_blank">
-              <b-icon icon="github"></b-icon>
-              <span>Github</span>
-            </a>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column has-text-centered">
+            <img class="animated fadeInDown" src="/static/favicon/favicon.png" height="250" width="250" alt="CIDR Blocker" style="margin-left:50px" />
           </div>
         </div>
+        <div class="has-text-centered">
+          <h1 class="title animated fadeIn">Blocks CIDR (Classless Inter-Domain Routing) IP Ranges</h1>
+          <a class="button is-primary is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/releases" target="_blank">
+            <b-icon icon="download"></b-icon>
+              <span>Download</span>
+            </a>
+          <a class="button is-info is-large" href="https://github.com/CIDR-Blocker/CIDR-Blocker/" target="_blank">
+            <b-icon icon="github"></b-icon>
+            <span>Github</span>
+          </a>
+        </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
